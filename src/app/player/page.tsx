ca1313b8715a57d8eb28b72b9158f64d3d0f4813
Сергей «Smile» Ливанский <Smilenoir@gmail.com>
2025-04-14 17:20:04 +0000

    Сделай Player Page по дизайну аналогично Admin Page

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -1,10 +1,29 @@
 'use client'
 
+import {Button} from "@/components/ui/button";
+import {ArrowLeft} from "lucide-react";
+import {useRouter} from "next/navigation";
+
 export default function PlayerPage() {
+  const router = useRouter();
+
   return (
-    <div>
-      <h1>Player Page</h1>
+    <div className="flex flex-col items-center min-h-screen py-2 bg-gray-900 text-white">
+      <div className="absolute bottom-4 left-4">
+        <Button
+          variant="outline"
+          className="h-10 w-10 p-0 bg-black border-gray-500 text-white rounded-full"
+          onClick={() => router.push('/')}
+        >
+          <ArrowLeft
+            className="h-6 w-6"
+            aria-hidden="true"
+          />
+        </Button>
+      </div>
+      <h1 className="text-3xl font-bold mb-4">Player Page</h1>
       <p>This is the player page.</p>
     </div>
   );
 }
+
